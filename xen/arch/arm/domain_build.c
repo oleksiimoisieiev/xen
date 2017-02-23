878dd886e60f1a597c6b10f882507660ca2a18a1
Andrii Anisov <andrii_anisov@epam.com>
2017-02-23 20:18:08 +0200

    dt/scf: introduce a dt helper to check if device is shared
    Introduce an unambiguous device tree helper to check if a device is intended to
    be shared using coprocessor sharing framework.
    
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>
    (Cherry-picked from 53ccf7fddaa94db1730f33af37f4cc270fedf5d3)
    Signed-off-by: Anastasiia Lukianenko <anastasiia_lukianenko@epam.com>

--- xen/arch/arm/domain_build.c
+++ xen/arch/arm/domain_build.c
@@ -1573,7 +1573,7 @@ static int __init handle_node(struct domain *d, struct kernel_info *kinfo,
         return make_timer_node(kinfo);
 
 #ifdef CONFIG_HAS_COPROC
-    if ( device_get_class(node) == DEVICE_COPROC )
+    if ( dt_device_for_coproc(node) )
     {
         res = handle_coproc_node(d, node);
         if ( res)
