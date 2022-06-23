d2d95c0c8f8ce4f084c743fb24c6485a238736a4
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2022-06-23 19:37:06 +0300

    fixed format error
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- xen/arch/arm/cpufreq/imx_sc_thermal.c
+++ xen/arch/arm/cpufreq/imx_sc_thermal.c
@@ -101,7 +101,7 @@ static int imx_sc_thermal_get_temp(void *data, int *temp)
 
 	*temp = GET_TEMP(celsius, tenths);
 
-    printk(XENLOG_INFO "<<< %s %d temp = %ld\n", __func__, __LINE__, temp);
+    printk(XENLOG_INFO "<<< %s %d temp = %d\n", __func__, __LINE__, *temp);
 
 	return 0;
 }
