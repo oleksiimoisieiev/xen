6c7087ecdc4c58c32bc90d2a4c99a845c72a0b28
Mykyta Poturai <mykyta_poturai@epam.com>
2022-06-24 14:41:03 +0300

    vscmi: Fix debug build
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>

--- xen/arch/arm/vscmi.c
+++ xen/arch/arm/vscmi.c
@@ -374,7 +374,7 @@ bool vscmi_handle_call(struct cpu_user_regs *regs)
         return false;
     }
 
-    flush_page_to_ram(scmi_mfn, false);
+    flush_page_to_ram(mfn_x(scmi_mfn), false);
     res = access_guest_memory_by_ipa(current->domain, scmi_ipa, data,
                                      VSCMI_SHM_SIZE, false);
     if ( res )
@@ -408,7 +408,7 @@ bool vscmi_handle_call(struct cpu_user_regs *regs)
     res = access_guest_memory_by_ipa(current->domain, scmi_ipa, data,
                                      VSCMI_SHM_SIZE, true);
 
-    flush_page_to_ram(scmi_mfn, false);
+    flush_page_to_ram(mfn_x(scmi_mfn), false);
     if ( res )
     {
         gprintk(XENLOG_ERR, "Error writing guest memory %d\n", res);
